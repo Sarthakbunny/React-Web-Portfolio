3850aca525cc9b767f08d25994b0ed05b599e2fa
Sarthak Srivastava <sasrivastava@Sarthaks-MacBook-Air.local>
2022-05-29 00:56:11 +0530

    adding timeline npm package

--- src/Components/Journey.js
+++ src/Components/Journey.js
@@ -1,4 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import CollegeIcon from '../images/collegeicon.png';
+import SchoolIcon from '../images/schoolicon.png';
+import JobIcon from '../images/jobicon.png';
+import StartIcon from '../images/starticon.png';
+import ZyngaIcon from '../images/zynga.png'
+import NagarroIcon from '../images/nagarro.jpeg';
+import BrandieIcon from '../images/brandie.jpeg';
+import IITIcon from '../images/iitrpr.jpeg';
+import NccuIcon from '../images/nccu.jpeg';
+
+const Icon = ({ src }) => {
+  return (
+    <div>
+      <img
+        className="img_logo"
+        src={src}
+        style={{
+          border: "1px solid #d1c8c8"
+        }}
+      />
+    </div>
+  )
+}
 /* array of object
 {
     year: 
@@ -13,7 +38,88 @@ tooltip when hover displaying JourneyTile compnent left right alternatively
 */
 function Journey() {
   return (
-    <div>Journey</div>
+    <div>
+      <VerticalTimeline lineColor='black'>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="2011 - present"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<Icon src={ZyngaIcon} />}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2010 - 2011"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<Icon src={NagarroIcon} />}
+        >
+          <h3 className="vertical-timeline-element-title">Art Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2008 - 2010"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<Icon src={BrandieIcon} />}
+        >
+          <h3 className="vertical-timeline-element-title">Web Designer</h3>
+          <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+          <p>
+            User Experience, Visual Design
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2006 - 2008"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<Icon src={NccuIcon} />}
+        >
+          <h3 className="vertical-timeline-element-title">Web Designer</h3>
+          <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+          <p>
+            User Experience, Visual Design
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="April 2013"
+          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+          icon={<Icon src={IITIcon} />}
+        >
+          <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
+          <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+          <p>
+            Strategy, Social Media
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="November 2012"
+          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+          icon={<Icon src={SchoolIcon} />}
+        >
+          <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
+          <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+          icon={<Icon src={StartIcon} />}
+        />
+      </VerticalTimeline>
+    </div>
   )
 }
 
